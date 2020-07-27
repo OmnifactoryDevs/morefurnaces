aed9538bf390f9ad0d1876191573d249dbcf5737
Neeve01 <winwyv@gmail.com>
2020-07-27 16:25:16 +0300

    Setup CI

--- src/main/java/cubex2/mods/morefurnaces/ModInformation.java
+++ src/main/java/cubex2/mods/morefurnaces/ModInformation.java
@@ -3,6 +3,6 @@ package cubex2.mods.morefurnaces;
 public class ModInformation
 {
     public static final String ID = "morefurnaces";
-    public static final String NAME = "More Furnaces: Omnifactory Edition";
+    public static final String NAME = "GRADLE:MODNAME";
     public static final String VERSION = "GRADLE:VERSION";
 }
